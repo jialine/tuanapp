e110732343f89b8aa4cd13eccf1fed276a1a0596
Michael.Lee <leewind19841209@gmail.com>
2013-12-10 12:12:45 +0800

    [dev]将tab换成2格空格

--- src/ui/c.ui.loading.js
+++ src/ui/c.ui.loading.js
@@ -1,28 +1,28 @@
 ﻿define(['libs', 'cBase', 'cUICore'], function (libs, cBase, cUICore) {
 
-	var options = {};
+  var options = {};
 
-	var _config = {
-		prefix: 'cui-'
-	};
+  var _config = {
+    prefix: 'cui-'
+  };
 
   var _attributes = {};
 
-	_attributes.class = _config.prefix + 'loading';
+  _attributes.class = _config.prefix + 'loading';
 
-	_attributes.onCreate = function () { };
+  _attributes.onCreate = function () { };
 
-	_attributes.onShow = function () {
-	  this.contentDom.html('<div class="cui-breaking-load"><div class="cui-w-loading"></div><i class="cui-white-logo"></i></div>');
-	  this.reposition();
-	};
-
-	options.__propertys__ = function () { };
-
-	options.initialize = function ($super) {
-		$super(_attributes);
+  _attributes.onShow = function () {
+    this.contentDom.html('<div class="cui-breaking-load"><div class="cui-w-loading"></div><i class="cui-white-logo"></i></div>');
+    this.reposition();
   };
 
-	return new cBase.Class(cUICore.Layer, options);
+  options.__propertys__ = function () { };
+
+  options.initialize = function ($super) {
+    $super(_attributes);
+  };
+
+  return new cBase.Class(cUICore.Layer, options);
 
 });
