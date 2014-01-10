d156cc0d8bfbf5f80bc6499b36853339064a4c18
l_wang <l_wang@ctrip.com>
2014-01-10 20:58:40 +0800

    重构

---
+++ src/business/c.business.app.js
@@ -0,0 +1,22 @@
+﻿define(['libs', 'cBase', 'AbstractAPP', 'cWidgetFactory', 'cWidgetGuider'], function (libs, cBase, AbstractAPP, WidgetFactory) {
+
+  var Appliction = new cBase.Class(AbstractAPP, {
+    __propertys__: function () {
+      
+    },
+    initialize: function ($super, options) {
+
+      $super(options);
+
+      //适配app 张爸爸
+      var Guider = WidgetFactory.create('Guider');
+      Guider.create();
+
+      //l_wang提升响应速度
+      $.bindFastClick && $.bindFastClick();
+
+    }
+
+  });
+  return Appliction;
+});
