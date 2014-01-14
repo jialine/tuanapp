88bed9619b63e0ef3b45fdaac81f95ad69a23148
l_wang <l_wang@ctrip.com>
2014-01-14 13:27:04 +0800

    重构

--- src/ui/c.ui.animation.js
+++ src/ui/c.ui.animation.js
@@ -1,156 +1,60 @@
 ﻿define([],
-function () {
+    function () {
 
-    return {
-        //        slideleft: function (inView, outView, callback, scope) {
-        //            this.body.addClass('hiddenx');
-        //            inView.$el.addClass('animatestart');
-        //            outView.$el.addClass('slideleftout');
-        //            inView.$el.addClass('sliderightin');
-        //            var self = this;
-        //            return setTimeout(function () {
-        //                self.body.removeClass('hiddenx');
-        //                inView.$el.removeClass('animatestart');
-        //                inView.$el.removeClass('sliderightin');
-        //                inView.$el.show();
-        //                outView.$el.removeClass('slideleftout');
-        //                outView.$el.hide();
-        //                callback && callback.call(scope);
-        //            }, 700);
-        //        },
-        //        slideright: function (inView, outView, callback, scope) {
-        //            this.body.addClass('hiddenx');
-        //            inView.$el.addClass('animatestart');
-        //            outView.$el.addClass('sliderightout');
-        //            inView.$el.addClass('slideleftin');
-        //            var self = this;
-        //            return setTimeout(function () {
-        //                self.body.removeClass('hiddenx');
-        //                inView.$el.removeClass('animatestart');
-        //                inView.$el.removeClass('slideleftin');
-        //                inView.$el.show();
-        //                outView.$el.removeClass('sliderightout');
-        //                outView.$el.hide();
-        //                callback && callback.call(scope);
-        //            }, 700);
-        //        },
+        return {
+            slideleft: function (inView, outView, callback, scope) {
+                inView.addClass('animatestart');
+                inView.addClass('sliderightin');
 
-        //以下为复写
-        //                slideleft: function (inView, outView, callback, scope) {
-        //                    this.body.addClass('hiddenx');
-        //                    var self = this;
-        //                    inView.$el.addClass('animatestart');
-        //                    inView.$el.css({
-        //                        '-webkit-transform': 'translate3d(100%, 0px, 0px)',
-        //                        '-moz-transform': 'translate3d(100%, 0px, 0px)'
-        //                    });
+                inView.__show();
 
-        //                    inView.$el.animate({
-        //                        '-webkit-transform': 'translate3d(0px, 0px, 0px)',
-        //                        '-moz-transform': 'translate3d(0px, 0px, 0px)'
-        //                    }, 300, 'linear', function () {
-        //                        self.body.removeClass('hiddenx');
-        //                        inView.$el.removeClass('animatestart');
-        //                        outView.$el.hide();
-        //                        callback && callback.call(scope);
-        //                    })
-        //                },
-        //                slideright: function (inView, outView, callback, scope) {
-        //                    this.body.addClass('hiddenx');
-        //                    var self = this;
-        //                    outView.$el.addClass('animatestart');
-        //                    outView.$el.css({
-        //                        '-webkit-transform': 'translate3d(0%, 0px, 0px)',
-        //                        '-moz-transform': 'translate3d(0%, 0px, 0px)'
-        //                    });
-        //                    outView.$el.animate({
-        //                        '-webkit-transform': 'translate3d(100%, 0px, 0px)',
-        //                        '-moz-transform': 'translate3d(100%, 0px, 0px)'
-        //                    }, 300, 'linear', function () {
-        //                        self.body.removeClass('hiddenx');
-        //                        outView.$el.removeClass('animatestart');
-        //                        outView.$el.hide();
-        //                        callback && callback.call(scope);
-        //                    });
-        //                },
+                var self = this;
+                return setTimeout(function () {
+                    inView.removeClass('animatestart');
+                    inView.removeClass('sliderightin');
 
-//        slideleft: function (inView, outView, callback, scope) {
-//            this.body.addClass('hiddenx');
-//            inView.$el.addClass('animatestart');
-//            inView.$el.addClass('sliderightin');
+                    if (outView) outView.__hide();
 
-//            outView.$el.addClass('animatestart');
-//            outView.$el.addClass('slideleftout');
+                    callback && callback.call(scope, inView, outView);
+                }, 340);
+            },
+            slideright: function (inView, outView, callback, scope) {
 
-//            var self = this;
-//            return setTimeout(function () {
-//                self.body.removeClass('hiddenx');
-//                inView.$el.removeClass('animatestart');
-//                inView.$el.removeClass('sliderightin');
+                if (outView) {
+                    outView.addClass('animatestart');
+                    outView.addClass('sliderightout');
+                }
 
-//                outView.$el.removeClass('animatestart');
-//                outView.$el.removeClass('slideleftout');
+                inView.__show();
 
-//                outView.$el.hide();
-//                callback && callback.call(scope);
-//            }, 390);
-//        },
+                var self = this;
+                return setTimeout(function () {
 
-//        slideright: function (inView, outView, callback, scope) {
-//            this.body.addClass('hiddenx');
-//            inView.$el.addClass('animatestart');
-//            inView.$el.addClass('slideleftin');
+                    if (outView) {
+                        outView.removeClass('animatestart');
+                        outView.removeClass('sliderightout');
+                        outView.__hide();
+                    }
 
-//            outView.$el.addClass('animatestart');
-//            outView.$el.addClass('sliderightout');
+                    callback && callback.call(scope, inView, outView);
 
-//            var self = this;
-//            return setTimeout(function () {
-//                self.body.removeClass('hiddenx');
-//                inView.$el.removeClass('animatestart');
-//                inView.$el.removeClass('slideleftin');
+                }, 340);
+            },
 
-//                outView.$el.removeClass('animatestart');
-//                outView.$el.removeClass('sliderightout');
 
-//                outView.$el.hide();
-//                callback && callback.call(scope);
-//            }, 390);
-//        },
+            noAnimate: function (inView, outView, callback, scope) {
+                //减少重绘和回流
+                this.mainframe.hide();
 
-        slideleft: function (inView, outView, callback, scope) {
-            inView.$el.addClass('animatestart');
-            inView.$el.addClass('sliderightin');
-            var self = this;
-            return setTimeout(function () {
-                inView.$el.removeClass('animatestart');
-                inView.$el.removeClass('sliderightin');
-                outView.$el.hide();
-                callback && callback.call(scope);
-              }, 340);
-        },
-        slideright: function (inView, outView, callback, scope) {
+                //in 一定会有 out则不一定
+                if (outView) outView.__hide();
+                inView.__show();
 
-            outView.$el.addClass('animatestart');
-            outView.$el.addClass('sliderightout');
+                this.mainframe.show();
 
-            var self = this;
-            return setTimeout(function () {
-                outView.$el.removeClass('animatestart');
-                outView.$el.removeClass('sliderightout');
-                outView.$el.hide();
+                callback && callback.call(scope, inView, outView);
 
-                callback && callback.call(scope);
-            }, 340);
-        },
+            }
 
-        fadeIn: function (inView, outView, callback, scope) {
-            this.mainframe.hide();
-            //原逻辑存在两个view可能同时出现在页面中的bug，这里强制先将所有view隐藏，在显示当前view
-            this.viewport.children('.sub-viewport').hide();
-            inView.$el.show();
-            this.mainframe.show();
-            callback && callback.call(scope || this);
-        }
-    };
-});
+        };
+    });
