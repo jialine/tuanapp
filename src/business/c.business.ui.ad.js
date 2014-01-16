ec8931c4fb3b36647ace298d901aaf810ecfc8b0
Michael.Lee <leewind19841209@gmail.com>
2014-01-16 11:15:51 +0800

    [dev] 加入business文件

---
+++ src/business/c.business.ui.ad.js
@@ -0,0 +1,47 @@
+﻿define(['cCoreInherit', 'cAdView'], function (cCoreInherit, AdView) {
+
+  var options = {};
+
+  options.checkForceDownload = function (sourceid) {
+    var self = this;
+    if (!sourceid || sourceid.length <= 0 || +sourceid <= 0) return;
+    //获取渠道信息
+    var s = adOptions._get("SALES_OBJECT");
+    var appUrl = AppUtility.appProtocol;
+    if (s && s.sid && +s.sid > 0) {
+      appUrl += '?extendSourceID=' + s.sid;
+    } else {
+      appUrl += '?extendSourceID=8888';
+    }
+    //传入处理函数，第一个是有app时候处理方案，第二个是没有app时候处理方案
+    //安装app情况下，第一个参数为true才会打开app，但是初次无论如何都会打开
+    AppUtility.openApp(function () {
+      adOptions.saveExpire();
+      self.hide();
+      return true;
+    }, function () {
+      var u = navigator.userAgent ? navigator.userAgent.toLocaleLowerCase() : '';
+      var isAndroid = (u.indexOf("android", 0) != -1) || (u.indexOf("adr", 0) != -1) ? 1 : 0;
+      //Android强制下载
+      if (isAndroid) {
+        var url = "http://m.ctrip.com/market/download.aspx?from=" + sourceid + '&App=3';
+
+        if (AppConfigMap && AppConfigMap.ad_download) {
+          if (AppConfigMap.ad_download.url) url = AppConfigMap.ad_download.url;
+        }
+
+        if (s && s.sid && +s.sid > 0 && +s.sid == +sourceid && s.appurl && s.appurl.length > 0) {
+          url = s.appurl;
+        }
+        adOptions.saveExpire();
+        self.hide();
+        window.location.href = url;
+      }
+    }, appUrl);
+  };
+  
+  var BusinessAdView = new cCoreInherit.extend(AdView, options);
+
+  return BusinessAdView;
+
+});
---
+++ src/business/c.business.widget.headerview.js
@@ -0,0 +1,40 @@
+﻿define(['cCoreInherit', 'cWidgetFactory', 'cWidgetHeaderView'], function (cCoreInherit, WidgetFactory) {
+
+	var HeaderView = WidgetFactory.create('HeaderView');
+
+	var WIDGET_NAME = 'Business.HeaderView';
+
+	var options = {};
+
+	options.set = function (data) {
+		if (data) {
+
+			if (AppConfigMap && AppConfigMap.page_header) {
+				if (typeof AppConfigMap.page_header === 'string') {
+					AppConfigMap.page_header = JSON.parse(AppConfigMap.page_header);
+				}
+
+				for (var key in AppConfigMap.page_header) {
+					if (data.data.hasOwnProperty(key)) {
+						data.data[key] = AppConfigMap.page_header[key];
+					}
+				}
+			}
+
+			this.data = data;
+			_setTemplate.call(this, { data: data });
+
+			this.bindEvents = this.data.bindEvents;
+			this.isCreate = false;
+			this.hide();
+		}
+	};
+
+	var BusinessHeaderView = new cCoreInherit.Class(HeaderView, options);
+
+	WidgetFactory.register({
+		name: WIDGET_NAME,
+		fn: BusinessHeaderView
+	});
+
+})
